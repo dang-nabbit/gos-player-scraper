fd2b0ec058a0d0af2c62ede1feeeb9b77dc4a9b0
dang-nabbit <34625456+dang-nabbit@users.noreply.github.com>
2018-02-25 14:22:52 -0300

    Upload script with support for last level
    - Add actual script to repository.
    - Add last level info to output.

---
+++ gos-scraper.user.js
@@ -0,0 +1,68 @@
+// ==UserScript==
+// @name         GOS scraper
+// @namespace    https://www.gatesofsurvival.com
+// @version      0.80
+// @description  try to take over the world!
+// @author       Opal
+// @match        https://www.gatesofsurvival.com/game/index.php?page=main
+// @grant        none
+// @downloadURL  https://github.com/dang-nabbit/gos-scraper/gos-scraper.user.js
+// ==/UserScript==
+    extraButtonCode = "<div id=\"extra2\" class = \"classy\"><li class=\"classy\">Opal's Addons</li></div>";
+    outputButtonCode ="<div id=\"output2\" class = \"classy\"><li class=\"classy\">---</li></div>";
+    outputButton2Code ="<div id=\"output3\" class = \"classy\"><li class=\"classy\">---</li></div>";
+    $("#logout").after(extraButtonCode);
+    $("#extra2").after(outputButtonCode);
+    $("#output2").after(outputButton2Code);
+    $("#extra2").click(function(){
+        var textNodes = $("#page > center table[width='80%'] tr").children('*').contents();
+        var arr = ["0"];
+        var skillTitles = [];
+        var skillValues = [];
+
+
+
+        var transferIndex = 0;
+        $.each(textNodes, function(index, value){
+
+            var zoop = $(value);
+            var zoopText = zoop.text().trim();
+            if (value.nodeType === 1 && (zoop.html().indexOf('Level') > -1))
+            {
+                arr.push(zoopText);
+                skillTitles.push(zoopText);
+            } else if (value.nodeType === 3 && zoopText !== '' && zoopText !== '\n'){
+                //&& (arr[arr.length-1].indexOf('Level') > -1)
+                console.log(zoopText);
+                zoop = zoopText.split(",").join("");
+                var maxValue = /\s\d+\s(?!\/)/;
+                var found = zoop.match(maxValue);
+                if (found){
+                    zoop = found[0];
+                }
+                skillLevel = zoop.replace(": ","");
+                arr.push(skillLevel);
+                skillValues.push(skillLevel);
+            }
+        });
+
+        zoop = $("#page > center i b");
+        if (zoop.length > 0) {
+            zoopText = zoop.first().text();
+            skillValues.push(zoopText);
+            console.log(zoopText);
+        }
+
+        var outputForm = "<form><input type = \"text\" id=\"outputform\" value=\"\">";
+        var outputForm3 = "<form><input type = \"text\" id=\"outputform3\" value=\"\">";
+        arr.splice(0, 1);
+        var output = skillValues.join(",");
+        $("#output2").html(outputForm);
+        $("#output3").html(outputForm3);
+        $("#outputform").val((skillValues.join("\t")));
+        console.log(skillTitles);
+        $("#outputform3").val((skillTitles.join("\t")));
+    });
+
+
+
