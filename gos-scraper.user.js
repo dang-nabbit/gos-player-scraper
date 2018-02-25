3f65fb4abf3db5211411cfec41f5cfdbd7c443f0
dang-nabbit <34625456+dang-nabbit@users.noreply.github.com>
2018-02-25 15:46:00 -0300

    Output empty string when no latest level
    Output empty string on the latest level field when there's no recent levels.

--- gos-scraper.user.js
+++ gos-scraper.user.js
@@ -3,7 +3,6 @@
 // @namespace    https://www.gatesofsurvival.com
 // @version      0.80
 // @description  try to take over the world!
-// @homepage     https://github.com/dang-nabbit/gos-scraper
 // @author       Opal
 // @match        https://www.gatesofsurvival.com/game/index.php?page=main
 // @grant        none
@@ -50,10 +49,11 @@
         zoop = $("#page > center i");
         if (zoop.length > 0) {
             zoopText = zoop.first().text();
-            if (zoopText !== 'Total Logins') {
-                skillValues.push(zoopText);
-                console.log(zoopText);
+            if (zoopText === 'Total Logins') {
+                zoopText = '';
             }
+            skillValues.push(zoopText);
+            console.log(zoopText);
         }
 
         var outputForm = "<form><input type = \"text\" id=\"outputform\" value=\"\">";
